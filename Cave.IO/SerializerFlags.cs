92ba42ce46b5f295cd16388b846af08e7214ed5b
Andreas Rohleder <andreas@rohleder.cc>
2021-11-06 16:36:50 +0100

    Added BinarySerializer: Provides fast and efficient field and property de-/serialization.

---
+++ Cave.IO/SerializerFlags.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Cave.IO
+{
+    [Flags]
+    public enum SerializerFlags
+    {
+        /// <summary>
+        /// No flags set.
+        /// </summary>
+        None = 0,
+
+        /// <summary>
+        /// Set this flag to de-/serialize private fields/properties
+        /// </summary>
+        NonPublic = 1 << 0,
+
+        /// <summary>
+        /// Set this flag to de-/serialize public fields/properties
+        /// </summary>
+        Public = 1 << 1,
+
+        /// <summary>
+        /// Set this flag to de-/serialize fields
+        /// </summary>
+        Fields = 1 << 2,
+
+        /// <summary>
+        /// Set this flag to de-/serialize properties
+        /// </summary>
+        Properties = 1 << 3,
+
+        /// <summary>
+        /// Set this flag to de-/serialize with type name
+        /// </summary>
+        TypeName = 1 << 4,
+
+        /// <summary>
+        /// Set this flag to de-/serialize with assembly name
+        /// </summary>
+        AssemblyName = 1 << 5,
+    }
+}
