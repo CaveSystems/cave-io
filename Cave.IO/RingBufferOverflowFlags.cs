c119a6e7b2696f7f4117b156b2f0ab746d7fca88
Andreas Rohleder <39777727+Dingsd4@users.noreply.github.com>
2023-06-09 14:50:14 +0200

    Updated IRingBuffer implementations

---
+++ Cave.IO/RingBufferOverflowFlags.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+#nullable enable
+
+namespace Cave.IO;
+
+/// <summary>
+/// Provides flags for overflow handling at <see cref="IRingBuffer{TValue}"/> implementations.
+/// </summary>
+[Flags]
+public enum RingBufferOverflowFlags
+{
+    /// <summary>
+    /// No specifal handling. The RingBuffer is unprotected, unchecked and will ignore overflows.
+    /// </summary>
+    None = 0,
+
+    /// <summary>
+    /// Prevent overflows by rejecting writes.
+    /// </summary>
+    Prevent = 1 << 0,
+
+    /// <summary>
+    /// Write to <see cref="Trace"/> on overflows.
+    /// </summary>
+    Trace = 1 << 1,
+
+    /// <summary>
+    /// Throw an <see cref="OverflowException"/> on overflows.
+    /// </summary>
+    Exception = 1 << 2,
+}
